58f4a8fdc28c21dbf587018658dac314b0be1bfc
Shashwat Mishra <144282119+smx64@users.noreply.github.com>
2023-09-22 01:58:06 -0400

    Coding + Sub-sites for HW03C

---
+++ 1000P/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background('#121212');
+}
+
+function draw() {
+  fill('#EBEBEB');
+  noStroke();
+  let dim = 30;
+  let maxCircles = 1000;
+  randomSeed(2);
+  for(let i=1;i<=maxCircles;i++)
+  {
+    ellipse(random(width), random(height), dim);
+  }
+}
